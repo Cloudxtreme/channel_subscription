df62ec9953b7dcb15e847085e988723a71b08a0c
Felipe Cavani <fcavani@gmail.com>
2015-12-03 06:40:59 -0200

    comments

--- channel.go
+++ channel.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by Apache 2.0
 // license that can be found in the LICENSE file.
 
+// Package channel_subscription provides a way to send from 1 to many channels.
 package channel_subscription
 
 import (
@@ -10,10 +11,14 @@ import (
 	"sync"
 )
 
+//ErrNotFound is an error.
 var ErrNotFound = errors.New("subscription not found")
 
+// ChannelSubscription map stores all channels to send.
 type ChannelSubscription map[reflect.Value]bool
 
+// Subscribe subscribes an remote channel that will send data to ch channel.
+// ch channel must be of one type only.
 func (c ChannelSubscription) Subscribe(ch interface{}) {
 	val := reflect.ValueOf(ch)
 	if val.Kind() != reflect.Chan {
@@ -22,6 +27,8 @@ func (c ChannelSubscription) Subscribe(ch interface{}) {
 	c[val] = true
 }
 
+// Unsubscribe removes the channel ch from the pool, nothing will be
+// send to it.
 func (c ChannelSubscription) Unsubscribe(ch interface{}) error {
 	val := reflect.ValueOf(ch)
 	_, found := c[val]
@@ -32,6 +39,9 @@ func (c ChannelSubscription) Unsubscribe(ch interface{}) error {
 	return nil
 }
 
+// TrySend sends data to the channels subscribeds. i type must be
+// compatible with ch type. The semantics is the same of the
+// TrySend method in the reflect.Value type.
 func (c ChannelSubscription) TrySend(i interface{}) (sent bool) {
 	sent = true
 	val := reflect.ValueOf(i)
@@ -41,6 +51,9 @@ func (c ChannelSubscription) TrySend(i interface{}) (sent bool) {
 	return
 }
 
+// Send sends data to the channels subscribeds. i type must be
+// compatible with ch type. The semantics is the same of the
+// Send method in the reflect.Value type.
 func (c ChannelSubscription) Send(i interface{}) {
 	val := reflect.ValueOf(i)
 	var wg sync.WaitGroup
